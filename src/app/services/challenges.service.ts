e53baf646f5bcb87e333aa8fdea5af4e63c3780f
magdykashef <magdy_kashef_777@yahoo.com>
2020-11-23 19:31:19 +0200

    added getAllChallenges method to get data from API

--- src/app/services/challenges.service.ts
+++ src/app/services/challenges.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { IChallenge } from './../shared/models/challenge';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChallengesService {
 
-  constructor() { }
+  private url = 'http://localhost:4200';
+
+  constructor(private http: HttpClient) { }
+
+
+  // get all challenges from database
+  getAllChallenges(): Observable<IChallenge[]> {
+    return this.http.get<IChallenge[]>(`${this.url}/challenges`);
+  }
+
 }
