7a002c88ee44eb86c479d9623c91aa8d457476fd
magdykashef <magdy_kashef_777@yahoo.com>
2020-11-23 18:17:46 +0200

    imported the SearchComponent

--- src/app/game-plan/game-plan.module.ts
+++ src/app/game-plan/game-plan.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { GamePlanComponent } from './game-plan.component';
+import { SearchComponent } from './../shared/components/search/search.component';
 
 
 @NgModule({
-  declarations: [GamePlanComponent],
+  declarations: [
+    GamePlanComponent,
+    SearchComponent
+  ],
   imports: [
     CommonModule,
     RouterModule
