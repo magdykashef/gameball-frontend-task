3be02da7faf014dc2b6b0e9a380571620d3e72f0
magdykashef <magdy_kashef_777@yahoo.com>
2020-11-25 23:34:35 +0200

    complete challenges component

--- src/app/challenges/challenges.component.ts
+++ src/app/challenges/challenges.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { EMPTY } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { ChallengesService } from '../services/challenges.service';
 
 @Component({
   selector: 'app-challenges',
   templateUrl: './challenges.component.html',
-  styleUrls: ['./challenges.component.scss']
+  styleUrls: ['./challenges.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ChallengesComponent implements OnInit {
+export class ChallengesComponent {
 
-  constructor() { }
+  errorMessage: string = '';
 
-  ngOnInit(): void {
-  }
+  //get challenges data form service
+  challenges$ = this.challengeservice.getAllChallenges$
+    .pipe(
+      catchError(error => {
+        this.errorMessage = error;
+        return EMPTY;
+      })
+    );
+
+  constructor(private challengeservice: ChallengesService) { }
 
 }
