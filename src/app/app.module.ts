33c0b6873882ce38f879082c145c14a48240a696
magdykashef <magdy_kashef_777@yahoo.com>
2020-11-25 23:21:31 +0200

    import HttpClintModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -18,6 +19,7 @@ import { QuestsModule } from './quests/quests.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     DashboardModule,
     GamePlanModule,
